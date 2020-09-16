0c676a3508b920f344e73a282c1bde4c472af9f9
mathiaslyngbye <malyn16@student.sdu.dk>
2020-09-16 21:56:44 +0200

    Make internal functions private

--- sudoku.h
+++ sudoku.h
@@ -8,14 +8,14 @@ class Sudoku
 public:
     Sudoku( std::array<std::array<int,9>,9> );
     bool verify();
+    void print();
+private:
     bool verifyRow(int);
     bool verifyRows();
     bool verifyCol(int);
     bool verifyCols();
     bool verifyBox(int, int);
     bool verifyBoxes();
-    void print();
-private:
     std::array<std::array<int,9>,9> map;
 };
 
